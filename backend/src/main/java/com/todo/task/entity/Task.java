d4e82b27377ad2d2a3ccf8e37be447ce21746f30
choiminu <alsdn4590@gmail.com>
2025-08-19 16:35:22 +0900

    feat(task) : Task 엔티티 생성 및 Repository 생성
    - Task의 상태를 관리할 수 있는 TaskStatus Enum 클래스 추가

---
+++ backend/src/main/java/com/todo/task/entity/Task.java
@@ -0,0 +1,46 @@
+package com.todo.task.entity;
+
+import com.todo.cateogry.domain.Category;
+import com.todo.user.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ backend/src/main/java/com/todo/task/entity/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.todo.task.entity;
+
+public enum TaskStatus {
+    NONE, PROGRESS, DONE
+}
