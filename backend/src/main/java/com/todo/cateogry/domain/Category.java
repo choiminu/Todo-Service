7899f6b08a62ba2a7aa541ef981f1f7ba6fb4da7
choiminu <alsdn4590@gmail.com>
2025-08-19 11:41:46 +0900

    feat(category): 카테고리 엔티티 생성
    - 카테고리는 id, name, user 속성을 가진다.
    - @ManyToOne fetch 타입 고민중.

---
+++ backend/src/main/java/com/todo/cateogry/domain/Category.java
@@ -0,0 +1,30 @@
+package com.todo.cateogry.domain;
+
+import com.todo.user.domain.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
