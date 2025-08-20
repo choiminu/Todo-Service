ebe7cc5f3f65cec4c4d9179d484a7567259913ba
choiminu <alsdn4590@gmail.com>
2025-08-20 17:43:16 +0900

    refactor(task): TaskStatus.from 기본 반환값을 NONE으로 변경
    - 기존에는 일치하는 값이 없을 경우 `null` 값을 반환하였지만 안정한 상태값을 보장하기 위해 시작전을 의미하는 (NONE)을 반환하도록 수정하였음

--- backend/src/main/java/com/todo/task/entity/TaskStatus.java
+++ backend/src/main/java/com/todo/task/entity/TaskStatus.java
@@ -3,13 +3,12 @@ package com.todo.task.entity;
 public enum TaskStatus {
     NONE, PROGRESS, DONE;
 
-
     public static TaskStatus from(String status) {
         for (TaskStatus value : TaskStatus.values()) {
             if (value.name().equalsIgnoreCase(status)) {
                 return value;
             }
         }
-        return null;
+        return TaskStatus.NONE;
     }
 }
