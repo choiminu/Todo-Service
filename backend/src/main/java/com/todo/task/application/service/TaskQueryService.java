2b6389a1788ebb1815ebd5a351132cf5aa0d847b
choiminu <alsdn4590@gmail.com>
2025-08-21 17:50:56 +0900

    refactor(category) : 주석삭제

--- backend/src/main/java/com/todo/task/application/service/TaskQueryService.java
+++ backend/src/main/java/com/todo/task/application/service/TaskQueryService.java
@@ -23,8 +23,6 @@ public class TaskQueryService {
     private final TaskMapper taskMapper;
 
     public List<TaskResponse> searchUserTasks(Long userId, TaskSearchRequest req) {
-
-        // 사용자 ID와 요청의 검색 조건(categoryId, 기간[startDate~endDate], 상태)을 기반으로 작업 목록을 조회한다.
         List<Task> tasks = taskRepository.search(
                 userId,
                 req.getCategoryId(),
@@ -32,8 +30,6 @@ public class TaskQueryService {
                 req.getEndDate(),
                 TaskStatus.from(req.getStatus())
         );
-
-        // TaskMapper 클래스를 통해 조회된 Task List를 응답 DTO로 변환하여 반환한다.
         return tasks.stream()
                 .map(taskMapper::toResponse)
                 .toList();
