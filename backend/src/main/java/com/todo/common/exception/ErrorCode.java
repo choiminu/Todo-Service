fa2431b465a49b3b3808158dab873346ed88a5ce
choiminu <alsdn4590@gmail.com>
2025-08-21 14:28:05 +0900

    feat(task): Task API Swagger 문서화 추가

--- backend/src/main/java/com/todo/common/exception/ErrorCode.java
+++ backend/src/main/java/com/todo/common/exception/ErrorCode.java
@@ -27,13 +27,13 @@ public enum ErrorCode {
     /**
      * 카테고리 관련 에러
      */
-    CATEGORY_NOT_FOUND(HttpStatus.BAD_REQUEST, "C01", "카테고리를 찾을 수 없습니다."),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "C01", "카테고리를 찾을 수 없습니다."),
     CATEGORY_UPDATE_FORBIDDEN(HttpStatus.BAD_REQUEST, "C02", "본인의 카테고리만 수정할 수 있습니다."),
 
     /**
      * Task 관련 에러
      */
-    TASK_NOT_FOUND(HttpStatus.BAD_REQUEST, "T01", "해당 Task를 찾을 수 없습니다."),
+    TASK_NOT_FOUND(HttpStatus.NOT_FOUND, "T01", "해당 Task를 찾을 수 없습니다."),
     TASK_ACCESS_FORBIDDEN(HttpStatus.BAD_REQUEST, "T02", "해당 Task의 접근 권한이 없습니다."),
     TASK_PERIOD_INVALID(HttpStatus.BAD_REQUEST, "T03", "유효하지 않은 기간입니다."),
 
@@ -44,7 +44,7 @@ public enum ErrorCode {
      */
     EMAIL_NOT_UNIQUE(HttpStatus.BAD_REQUEST, "U01", "이미 사용중인 이메일입니다."),
     PASSWORD_MISMATCH(HttpStatus.BAD_REQUEST, "U02", "비밀번호가 일치하지 않습니다."),
-    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "U03", "해당 사용자를 찾을 수 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U03", "해당 사용자를 찾을 수 없습니다."),
     LOGIN_FAIL(HttpStatus.BAD_REQUEST, "U04", "이메일 또는 아이디가 일치하지 않습니다.")
 
     ;
--- backend/src/main/java/com/todo/task/presentation/TaskApiDocs.java
+++ backend/src/main/java/com/todo/task/presentation/TaskApiDocs.java
@@ -1,11 +1,13 @@
-package com.todo.cateogry.presentation;
+package com.todo.task.presentation;
 
-import com.todo.cateogry.application.dto.CategoryRequest;
-import com.todo.cateogry.application.dto.CategoryResponse;
 import com.todo.common.response.ErrorResponse;
 import com.todo.common.response.SuccessResponse;
 import com.todo.common.session.LoginUser;
 import com.todo.common.session.resolver.Login;
+import com.todo.task.application.dto.request.TaskCreateRequest;
+import com.todo.task.application.dto.request.TaskSearchRequest;
+import com.todo.task.application.dto.request.TaskUpdateRequest;
+import com.todo.task.application.dto.response.TaskResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -15,32 +17,44 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@Tag(name = "Category", description = "카테고리 관련 API")
-public interface CategoryApiDocs {
+@Tag(name = "Task", description = "할 일(Task) 관련 API")
+public interface TaskApiDocs {
 
-    @Operation(summary = "카테고리 생성 API")
+    @Operation(summary = "할 일 생성 API")
     @ApiResponses({
-            @ApiResponse(responseCode = "201", description = "카테고리 생성 성공"),
+            @ApiResponse(responseCode = "201", description = "생성 성공"),
             @ApiResponse(
                     responseCode = "400",
                     description = "잘못된 요청",
                     content = @Content(
                             mediaType = "application/json",
                             schema = @Schema(implementation = ErrorResponse.class),
-                            examples = {
+                            examples = {@ExampleObject(
+                                    name = "요청 형식 오류",
+                                    value = "{\n"
+                                            + "  \"status\": 400,\n"
+                                            + "  \"code\": \"T01\",\n"
+                                            + "  \"message\": \"요청 값이 올바르지 않습니다.\"\n"
+                                            + "}"
+                            ), @ExampleObject(
+                                    name = "카테고리 없음",
+                                    value = "{\n"
+                                            + "  \"status\": 400,\n"
+                                            + "  \"code\": \"C01\",\n"
+                                            + "  \"message\": \"카테고리를 찾을 수 없습니다.\"\n"
+                                            + "}"
+                            ),
                                     @ExampleObject(
-                                            name = "요청 형식 오류",
+                                            name = "유효하지 않을 날짜",
                                             value = "{\n"
                                                     + "  \"status\": 400,\n"
-                                                    + "  \"code\": \"C01\",\n"
-                                                    + "  \"message\": \"요청 값이 올바르지 않습니다.\"\n"
+                                                    + "  \"code\": \"T03\",\n"
+                                                    + "  \"message\": \"유효하지 않은 기간입니다.\"\n"
                                                     + "}"
-                                    )
-                            }
+                                    )}
                     )
             ),
             @ApiResponse(
@@ -78,23 +92,106 @@ public interface CategoryApiDocs {
                     )
             )
     })
-    SuccessResponse<CategoryResponse> createCategory(
-            @Login LoginUser user,
+    SuccessResponse<TaskResponse> createTask(
+            @Login LoginUser loginUser,
             @RequestBody(
-                    description = "카테고리 생성 정보",
+                    description = "할 일 생성 정보",
                     content = @Content(
                             mediaType = "application/json",
-                            schema = @Schema(implementation = CategoryRequest.class),
+                            schema = @Schema(implementation = TaskCreateRequest.class),
                             examples = @ExampleObject(
                                     value = "{\n"
-                                            + "  \"name\": \"일상\"\n"
+                                            + "  \"categoryId\": 1,\n"
+                                            + "  \"title\": \"운동하기\",\n"
+                                            + "  \"content\": \"저녁 7시 러닝\",\n"
+                                            + "  \"startDate\": \"2025-08-21\",\n"
+                                            + "  \"endDate\": \"2025-08-21\"\n"
                                             + "}"
                             )
                     )
-            ) CategoryRequest request
+            ) TaskCreateRequest request
     );
 
-    @Operation(summary = "카테고리 목록 조회 API")
+    @Operation(summary = "할 일 단건 조회 API")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "잘못된 요청",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "존재하지 않은 Task",
+                                            value = "{\n"
+                                                    + "  \"status\": 400,\n"
+                                                    + "  \"code\": \"T01\",\n"
+                                                    + "  \"message\": \"해당 Task를 찾을 수 없습니다.\"\n"
+                                                    + "}"
+                                    )
+                            }
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "접근권한 부족",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "접근권한부족",
+                                            value = "{\n"
+                                                    + "  \"status\": 401,\n"
+                                                    + "  \"code\": \"U01\",\n"
+                                                    + "  \"message\": \"로그인이 필요한 서비스입니다.\"\n"
+                                                    + "}"
+                                    )
+                            }
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "대상 없음",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class),
+                            examples = @ExampleObject(
+                                    name = "할 일을 찾을 수 없음",
+                                    value = "{\n"
+                                            + "  \"status\": 404,\n"
+                                            + "  \"code\": \"T04\",\n"
+                                            + "  \"message\": \"할 일을 찾을 수 없습니다.\"\n"
+                                            + "}"
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "서버 내부 오류",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class),
+                            examples = @ExampleObject(
+                                    name = "서버 내부 오류",
+                                    value = "{\n"
+                                            + "  \"status\": 500,\n"
+                                            + "  \"code\": \"S01\",\n"
+                                            + "  \"message\": \"서버 내부 오류가 발생했습니다.\"\n"
+                                            + "}"
+                            )
+                    )
+            )
+    })
+    SuccessResponse<TaskResponse> findTask(
+            @Parameter(description = "조회할 Task ID", content = @Content(
+                    schema = @Schema(implementation = Long.class),examples = @ExampleObject(value = "1")
+            )) Long taskId,
+            @Login LoginUser loginUser
+    );
+
+    @Operation(summary = "할 일 검색 API")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "조회 성공"),
             @ApiResponse(
@@ -132,9 +229,26 @@ public interface CategoryApiDocs {
                     )
             )
     })
-    SuccessResponse<List<CategoryResponse>> findCategories(@Login LoginUser user);
+    SuccessResponse<List<TaskResponse>> searchTask(
+            @Login LoginUser loginUser,
+            @RequestBody(
+                    description = "할 일 검색 조건",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = TaskSearchRequest.class),
+                            examples = @ExampleObject(
+                                    value = "{\n"
+                                            + "  \"categoryId\": 1,\n"
+                                            + "  \"status\": \"NONE\",\n"
+                                            + "  \"startDate\": \"2025-08-21\",\n"
+                                            + "  \"endDate\": \"2025-08-21\"\n"
+                                            + "}"
+                            )
+                    )
+            ) TaskSearchRequest request
+    );
 
-    @Operation(summary = "카테고리 수정 API")
+    @Operation(summary = "할 일 수정 API")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "수정 성공"),
             @ApiResponse(
@@ -147,7 +261,7 @@ public interface CategoryApiDocs {
                                     name = "요청 형식 오류",
                                     value = "{\n"
                                             + "  \"status\": 400,\n"
-                                            + "  \"code\": \"C01\",\n"
+                                            + "  \"code\": \"T01\",\n"
                                             + "  \"message\": \"요청 값이 올바르지 않습니다.\"\n"
                                             + "}"
                             )
@@ -172,17 +286,17 @@ public interface CategoryApiDocs {
                     )
             ),
             @ApiResponse(
-                    responseCode = "400",
+                    responseCode = "404",
                     description = "대상 없음",
                     content = @Content(
                             mediaType = "application/json",
                             schema = @Schema(implementation = ErrorResponse.class),
                             examples = @ExampleObject(
-                                    name = "카테고리 없음",
+                                    name = "할 일을 찾을 수 없음",
                                     value = "{\n"
-                                            + "  \"status\": 400,\n"
-                                            + "  \"code\": \"C01\",\n"
-                                            + "  \"message\": \"카테고리를 찾을 수 없습니다.\"\n"
+                                            + "  \"status\": 404,\n"
+                                            + "  \"code\": \"T04\",\n"
+                                            + "  \"message\": \"할 일을 찾을 수 없습니다.\"\n"
                                             + "}"
                             )
                     )
@@ -204,44 +318,30 @@ public interface CategoryApiDocs {
                     )
             )
     })
-    SuccessResponse<CategoryResponse> updateCategory(
-            @Login LoginUser user,
-            @Parameter(description = "수정할 Category ID", content = @Content(
-                    schema = @Schema(implementation = Long.class), examples = @ExampleObject(value = "1")
-            )) Long categoryId,
+    SuccessResponse<TaskResponse> updateTask(
+            @Parameter(description = "수정할 Task ID", content = @Content(
+                    schema = @Schema(implementation = Long.class),examples = @ExampleObject(value = "1")
+            )) Long taskId,
+            @Login LoginUser loginUser,
             @RequestBody(
-                    description = "카테고리 수정 정보",
+                    description = "할 일 수정 정보",
                     content = @Content(
                             mediaType = "application/json",
-                            schema = @Schema(implementation = CategoryRequest.class),
+                            schema = @Schema(implementation = TaskUpdateRequest.class),
                             examples = @ExampleObject(
                                     value = "{\n"
-                                            + "  \"name\": \"업무\"\n"
+                                            + "  \"title\": \"운동하기(수정)\",\n"
+                                            + "  \"content\": \"러닝 → 헬스\",\n"
+                                            + "  \"status\": \"PROGRESS\"\n"
                                             + "}"
                             )
                     )
-            ) CategoryRequest request
+            ) TaskUpdateRequest request
     );
 
-    @Operation(summary = "카테고리 삭제 API")
+    @Operation(summary = "할 일 삭제 API")
     @ApiResponses({
             @ApiResponse(responseCode = "204", description = "삭제 성공"),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "대상 없음",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = ErrorResponse.class),
-                            examples = @ExampleObject(
-                                    name = "카테고리 없음",
-                                    value = "{\n"
-                                            + "  \"status\": 400,\n"
-                                            + "  \"code\": \"C01\",\n"
-                                            + "  \"message\": \"카테고리를 찾을 수 없습니다.\"\n"
-                                            + "}"
-                            )
-                    )
-            ),
             @ApiResponse(
                     responseCode = "401",
                     description = "접근권한 부족",
@@ -261,6 +361,22 @@ public interface CategoryApiDocs {
                     )
             ),
             @ApiResponse(
+                    responseCode = "404",
+                    description = "대상 없음",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class),
+                            examples = @ExampleObject(
+                                    name = "할 일을 찾을 수 없음",
+                                    value = "{\n"
+                                            + "  \"status\": 404,\n"
+                                            + "  \"code\": \"T04\",\n"
+                                            + "  \"message\": \"할 일을 찾을 수 없습니다.\"\n"
+                                            + "}"
+                            )
+                    )
+            ),
+            @ApiResponse(
                     responseCode = "500",
                     description = "서버 내부 오류",
                     content = @Content(
@@ -277,8 +393,10 @@ public interface CategoryApiDocs {
                     )
             )
     })
-    SuccessResponse<Void> deleteCategory(@Login LoginUser user,
-                                         @Parameter(description = "삭제할 Category ID", content = @Content(
-                                                 schema = @Schema(implementation = Long.class), examples = @ExampleObject(value = "1")
-                                         )) Long categoryId);
+    SuccessResponse<Void> deleteTask(
+            @Parameter(description = "삭제할 Task ID", content = @Content(
+                    schema = @Schema(implementation = Long.class),examples = @ExampleObject(value = "1")
+            )) Long taskId,
+            @Login LoginUser loginUser
+    );
 }
