0a921a8fcda711ba6ca2a33d54cc816e0e2434c3
choiminu <alsdn4590@gmail.com>
2025-08-22 18:37:48 +0900

    fix(task): status 파라미터가 없을 경우 전체 상태를 조회하도록 수정
    - TaskSearchRequest 처리 로직 변경
    - status 미입력 시 NONE, PROGRESS, DONE 모두 포함되도록 조회

--- backend/src/main/java/com/todo/task/application/dto/request/TaskSearchRequest.java
+++ backend/src/main/java/com/todo/task/application/dto/request/TaskSearchRequest.java
@@ -1,5 +1,7 @@
 package com.todo.task.application.dto.request;
 
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,8 +9,15 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class TaskSearchRequest {
+    @Schema(description = "카테고리 ID", example = "1")
     private Long categoryId;
+
+    @Schema(description = "상태", example = "PROGRESS")
     private String status;
+
+    @Schema(description = "시작일", example = "2025-08-20")
     private LocalDate startDate;
+
+    @Schema(description = "종료일", example = "2025-08-25")
     private LocalDate endDate;
 }
